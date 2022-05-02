42aa1bff94f45f2d617182dc369654faf7cb19ad
Lee GunJun <ac9832@naver.com>
2022-05-02 22:11:04 +0900

    Comment 기능 추가

--- blog/client/src/PostList.js
+++ blog/client/src/PostList.js
@@ -1,9 +1,11 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios'
+import CommentCreate from './CommentCreate';
+import CommentList from './CommentList';
 
 // eslint-disable-next-line import/no-anonymous-default-export
 export default () => {
-    const [posts, setPosts] = useState('');
+    const [posts, setPosts] = useState({});
 
     const fetchPosts = async () => {
         const res = await axios.get('http://localhost:4000/posts');
@@ -24,6 +26,8 @@ export default () => {
             >
                 <div className="card-body">
                     <h3>{post.title}</h3>
+                    <CommentList postId={post.id} />
+                    <CommentCreate postId={post.id} />
                 </div>
             </div>
         );
