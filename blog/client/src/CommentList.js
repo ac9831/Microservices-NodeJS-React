8376823f3dc8dd3cfeea5a09c94b9e9333b5872f
Lee GunJun <ac9832@naver.com>
2022-05-05 22:38:22 +0900

    moderation, query 개발 진행

--- blog/client/src/CommentList.js
+++ blog/client/src/CommentList.js
@@ -1,19 +1,7 @@
-import React, { useState, useEffect } from "react";
-import axios from 'axios'
+import React from "react";
 
-export default ({ postId }) => {
-    const [comments, setComments] = useState([]);
-
-    const fetchData = async () => {
-        const res = await axios.get(`http://localhost:4001/posts/${postId}/comments`)
-
-        setComments(res.data);
-    }
-
-    useEffect(() => {
-        fetchData();
-    }, [])
-
+// eslint-disable-next-line import/no-anonymous-default-export
+export default ({ comments }) => {
     const renderedComments = comments.map(comment => {
         return <li key={comment.id}>{comment.content}</li>
     })
@@ -22,3 +10,4 @@ export default ({ postId }) => {
         {renderedComments}
     </ul>
 }
+
