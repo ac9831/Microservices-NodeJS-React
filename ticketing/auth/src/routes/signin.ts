3bc3a6c93ce7e367f1a792add33c11690cebfd82
Lee GunJun <ac9832@naver.com>
2022-06-22 22:44:19 +0900

    Auth 기능 마무리

--- ticketing/auth/src/routes/signin.ts
+++ ticketing/auth/src/routes/signin.ts
@@ -1,7 +1,11 @@
 import express, {Request, Response } from 'express'
-import { body, validationResult } from 'express-validator'
+import { body } from 'express-validator'
+import jwt from 'jsonwebtoken'
 
-import { RequestValidationError } from '../errors/request-validation-error'
+import { Password } from '../services/password'
+import { validateRequest } from '../middlewares/validate-request'
+import { User } from '../models/user'
+import { BadRequestError } from '../errors/bad-request-error'
 
 const router = express.Router()
 
@@ -15,12 +19,31 @@ router.post('/api/users/signin',
             .notEmpty()
             .withMessage('You must suppy a password')
     ],
-    (req: Request, res: Response) => {
-        const errors = validationResult(req)
+    validateRequest,
+    async (req: Request, res: Response) => {
+        const { email, password } = req.body
 
-        if (!errors.isEmpty()) {
-            throw new RequestValidationError(errors.array())
+        const existingUser = await User.findOne({ email })
+        if (!existingUser) {
+            throw new BadRequestError('Invalid Credentials -> Not Exisit User')
         }
+
+        const passwordsMatch = await Password.compare(existingUser.password, password)
+        if (!passwordsMatch) {
+            throw new BadRequestError('Invalid Credentials -> Password Not Match')
+        }
+
+        const userJwt = jwt.sign({
+                id: existingUser.id,
+                email: existingUser.email
+            }, process.env.JWT_KEY!
+        )
+
+        req.session = {
+            jwt: userJwt
+        }
+
+        res.status(201).send(existingUser)
     }
 )
 
