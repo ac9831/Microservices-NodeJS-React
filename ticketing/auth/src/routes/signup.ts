2c63ad3c39e92794590aa7b09644817c36c4146d
Lee GunJun <ac9832@naver.com>
2022-06-26 15:50:24 +0900

    남은 테스트 코드 완성

--- ticketing/auth/src/routes/signup.ts
+++ ticketing/auth/src/routes/signup.ts
@@ -22,7 +22,6 @@ router.post(
     validateRequest,
     async (req: Request, res: Response) => {
         const { email, password } = req.body
-        console.log(email, password)
 
         const existingUser = await User.findOne({ email })
 
