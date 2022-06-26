c752ac633bf125bbbe79c2a7dc478af091edc88f
Lee GunJun <ac9832@naver.com>
2022-06-26 23:47:58 +0900

    react client 개발 시작
    - 프로젝트 생성
    - deploy 수정

--- ticketing/auth/src/index.ts
+++ ticketing/auth/src/index.ts
@@ -3,13 +3,12 @@ import mongoose from 'mongoose'
 import { app } from './app'
 
 const start = async () => {
-    process.env.JWT_KEY = 'asdf'
     if (!process.env.JWT_KEY) {
         throw new Error('JWT_KEY must be defined')
     }
 
     try {
-        await mongoose.connect('mongodb://localhost:27017/auth')
+        await mongoose.connect('mongodb://auth-mongo-svc:27017/auth')
         console.log('Connected to MongoDb')
     } catch (err) {
         console.error(err)
